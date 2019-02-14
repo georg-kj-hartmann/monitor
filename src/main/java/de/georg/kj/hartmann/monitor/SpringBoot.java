068cac936299adf7d62a6673bf310a1e084afaf0
Georg Hartmann <georg.kj.hartmann@gmail.com>
2019-02-14 17:42:08 +0100

    spring boot basic

---
+++ src/main/java/de/georg/kj/hartmann/monitor/SpringBoot.java
@@ -0,0 +1,33 @@
+package de.georg.kj.hartmann.monitor;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringBoot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
+
+}
