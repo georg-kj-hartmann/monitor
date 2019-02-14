303237e1343de7a2943a0498ff8eddf7731a10f4
Georg Hartmann <georg.kj.hartmann@gmail.com>
2019-02-14 17:32:24 +0100

    inital project

---
+++ src/main/java/de/georg/kj/hartmann/monitor/App.java
@@ -0,0 +1,13 @@
+package de.georg.kj.hartmann.monitor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
