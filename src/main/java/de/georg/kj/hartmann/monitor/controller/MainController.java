0c8d650b1f8ec6d539770e451768be650eebea97
Georg Hartmann <georg.kj.hartmann@gmail.com>
2019-02-14 17:55:46 +0100

    spring boot web inital

--- src/main/java/de/georg/kj/hartmann/monitor/controller/MainController.java
+++ src/main/java/de/georg/kj/hartmann/monitor/controller/MainController.java
@@ -1,14 +1,14 @@
 package de.georg.kj.hartmann.monitor.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class MainController {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "home";
 
 	}
 }
